5657eecf4974cd605030f8bc71792e7b69d4a2ac
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-22 23:03:45 +0100

    fix login page updating user state, add loading state to prevent flashes when signed in user goes to log in route

--- src/app/routes/login.jsx
+++ src/app/routes/login.jsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
-import { login } from "../../services/authService";
+import { useContext, useEffect, useState } from "react";
+import { login } from "../../services/authService.js";
+import { AuthContext } from "../../services/authProvider.jsx";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
   const [errors, setErrors] = useState();
+  const { user, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home");
+    } else {
+      setLoading(false);
+    }
+  }, [user, navigate]);
 
   async function handleLogin(e) {
     try {
       e.preventDefault();
       const userLogin = await login(username, password);
       //if server returns error message, re-render and display
-      if (userLogin.error) {
-        setErrors(userLogin.error);
+      if (userLogin?.error) {
+        return setErrors(userLogin.error);
+      } else {
+        navigate("/home");
+        console.log("cya!");
+        return setUser(userLogin.user);
       }
     } catch (err) {
-      setErrors("Unexpected login error, please try again");
+      setErrors(err.message);
     }
   }
 
@@ -27,32 +44,34 @@ function Login() {
     }
   }
 
+  //prevent log-in screen flashing if user already logged in
+  if (loading) return;
+
   return (
-    <>
-      <div>
-        <h1>Login</h1>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={handleChange}
-          />
-          <button>Login</button>
-        </form>
-        <div>{errors}</div>
-      </div>
-    </>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
+      <div>{errors}</div>
+      <Link to="/sign-up"> Sign up! </Link>
+    </div>
   );
 }
 
