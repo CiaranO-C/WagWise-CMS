133e67cedbf56288162f8c10d3040d19382f00b8
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-26 15:22:09 +0100

    write protected page layout styling

--- src/app/routes/Protected.jsx
+++ src/app/routes/Protected.jsx
@@ -1,13 +1,25 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../services/authProvider';
-import { Navigate, Outlet } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../../services/authProvider";
+import { Navigate, Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Sidebar from '../components/Sidebar';
 
 function Protected() {
-   const {user} = useContext(AuthContext);
-   
+  const { user } = useContext(AuthContext);
+  
+  if (!user) return <Navigate to="/login" />;
+
   return (
-    user ? <Outlet/> : <Navigate to="/login" />
-  )
+    <ProtectedLayout>
+      <Sidebar />
+      <Outlet />
+    </ProtectedLayout>
+  );
 }
+
+const ProtectedLayout = styled.main`
+display: grid;
+grid-template-columns: 200px 1fr;
+`;
 
 export default Protected;
