0279da36c2d21fadf06ee225f54e31321b01ebed
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-28 23:58:49 +0100

    create in progress component - displays last updated article

---
+++ src/app/components/InProgress.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { Shared } from "../sharedStyles";
+import { useEffect, useState } from "react";
+
+function InProgress() {
+  const [article, setArticle] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchLastUpdated() {
+      const res = await fetch(
+        "/api/articles/admin/unpublished?sort=updated&limit=1",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        },
+      );
+      console.log(res);
+      const resJson = await res.json();
+      console.log(resJson);
+      
+      setArticle(resJson.articles[0]);
+      setLoading(false);
+      console.log(resJson);
+    }
+    fetchLastUpdated();
+  }, []);
+
+  if(loading) return <h1>Loading!</h1>
+
+  return (
+    <InProgressCard>
+      <h2>In Progress</h2>
+      <p>{article.title}</p>
+      <p>Last updated: {new Date(article.updated).toLocaleDateString()}</p>
+    </InProgressCard>
+  );
+}
+
+const InProgressCard = styled.section`
+  ${Shared}
+`;
+
+export default InProgress;
