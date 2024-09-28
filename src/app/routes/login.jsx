0c253ef34bcc7e4573277214e751f989bcf0d70d
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-09-28 13:21:45 +0100

    update styling and html structure

--- src/app/routes/login.jsx
+++ src/app/routes/login.jsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { login } from "../../services/authService.js";
 import { AuthContext } from "../../services/authProvider.jsx";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { Button } from "../sharedStyles.jsx";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -12,10 +13,11 @@ function Login() {
   const { user, setUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const usernameRef = useRef(null);
+  const submitRef = useRef(null);
 
   useEffect(() => {
-    if(usernameRef.current){
-        usernameRef.current.focus()
+    if (usernameRef.current) {
+      usernameRef.current.focus();
     }
   }, [loading]);
 
@@ -36,7 +38,6 @@ function Login() {
         return setErrors(userLogin.error);
       } else {
         navigate("/admin/home");
-        console.log("cya!");
         return setUser(userLogin.user);
       }
     } catch (err) {
@@ -53,60 +54,117 @@ function Login() {
   }
 
   function handleGuest() {
-    setUsername("Admin");
+    setUsername("admin");
     setPassword("adminPass1");
+    //defer submit til async state setters finished (adds to end of event queue)
+    setTimeout(() => {
+      if (submitRef) {
+        submitRef.current.click();
+      }
+    }, 0);
   }
 
   //prevent log-in screen flashing if user already logged in
   if (loading) return;
 
   return (
-    <main>
-      <LoginContainer>
-        <h1>Admin Login</h1>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={handleChange}
-            ref={usernameRef}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={handleChange}
-          />
-          <button>Login</button>
-        </form>
-        <button onClick={handleGuest}>Guest Account</button>
-        <div>{errors}</div>
-      </LoginContainer>
-    </main>
+    <>
+      <LoginMain>
+        <LoginContainer>
+          <h1>Admin Login</h1>
+          <form onSubmit={handleLogin}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={handleChange}
+              ref={usernameRef}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={handleChange}
+            />
+            <button ref={submitRef}>Login</button>
+          </form>
+          <button onClick={handleGuest}>Guest Account</button>
+          <div>{errors}</div>
+        </LoginContainer>
+        <img className="dog" src="/wagwise-dog.png" />
+      </LoginMain>
+    </>
   );
 }
+
+const LoginMain = styled.main`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #8eac6c;
+  padding: 0px 60px;
+
+  .dog {
+    max-height: 55vh;
+  }
+`;
 
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 20px 70px;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  gap: 10px;
+  width: 400px;
+  color: black;
 
   h1 {
     font-size: 2rem;
+    padding-bottom: 10px;
+    margin-bottom: 20px;
   }
 
   form {
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    margin-bottom: 10px;
+
+    input + label {
+      margin-top: 10px;
+    }
+
+    button {
+      margin-top: 20px;
+    }
+  }
+
+  label,
+  input {
+    transition: 0.3s;
+  }
+
+  label:has(+ input:focus) {
+    color: white;
+  }
+
+  input {
+    padding: 5px;
+    background-color: #ffffff00;
+    border: none;
+    border-bottom: 0.75px solid;
+    height: 30px;
+
+    &:focus {
+      outline: none;
+      border-bottom: 1.5px solid white;
+    }
+  }
+
+  button {
+    ${Button}
   }
 `;
 
