7b3619c1d4608fd4708e7fb0c49b24cb68813835
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-09-28 14:42:33 +0100

    add logo image, improve css

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 function Header() {
   return (
     <StyledHeader>
-      <h3>wag wise</h3>
+      <img src="/wagwise-logo.png" alt="wagwise logo" />
       <LinkContainer className="links">
         <a href="/*link to user front end*/">User Site</a>
         <a
@@ -20,12 +20,20 @@ function Header() {
 }
 
 const StyledHeader = styled.header`
-  background-color: rgb(225, 105, 34);
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0px 20px;
+      background-color: rgb(163 191 130);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 20px;
+    z-index: 1;
+    box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px;
+
+
+  img {
+    width: 112px;
+  }
 `;
+
 const LinkContainer = styled.div`
   display: flex;
   a {
