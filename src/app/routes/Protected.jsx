64bd08042c0da04bff53773a90b78a81f89ed82d
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-09-28 13:23:17 +0100

    change layout style

--- src/app/routes/Protected.jsx
+++ src/app/routes/Protected.jsx
@@ -2,12 +2,13 @@ import { useContext } from "react";
 import { AuthContext } from "../../services/authProvider";
 import { Navigate, Outlet } from "react-router-dom";
 import styled from "styled-components";
-import Sidebar from '../components/Sidebar';
+import Sidebar from "../components/Sidebar";
 
 function Protected() {
   const { user } = useContext(AuthContext);
-  
   if (!user) return <Navigate to="/login" />;
+
+console.log("Protected passed -->", user);
 
   return (
     <ProtectedLayout>
@@ -20,6 +21,8 @@ function Protected() {
 const ProtectedLayout = styled.main`
 display: grid;
 grid-template-columns: 200px 1fr;
+max-height: 100vh;
+}
 `;
 
 export default Protected;
