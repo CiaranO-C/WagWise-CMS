0a9f777cb6012a9607be153a2fabd91c021a0d52
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-09-30 12:33:20 +0100

    file restructure

---
+++ src/app/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import routesConfig from "./app/router";
+import { AuthProvider } from "./services/authProvider";
+
+const routes = createBrowserRouter(routesConfig);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={routes} />
+    </AuthProvider>
+  </StrictMode>,
+);
