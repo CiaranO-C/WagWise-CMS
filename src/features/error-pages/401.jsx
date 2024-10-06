c011bb553fbe411108ade79f23403224ff338c54
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-06 16:53:38 +0100

    move error pages to features directory

---
+++ src/features/error-pages/401.jsx
@@ -0,0 +1,22 @@
+import { MdBlock } from "react-icons/md";
+import styled from "styled-components";
+import { ErrorStatus } from "../../../components/sharedStyles";
+
+function Unauthorized() {
+  return (
+    <>
+      <StatusContainer>
+        <MdBlock />
+        <h1>401</h1>
+        <MdBlock />
+      </StatusContainer>
+      <p>You do not have access to this page!</p>
+    </>
+  );
+}
+
+const StatusContainer = styled.div`
+  ${ErrorStatus}
+`;
+
+export default Unauthorized;
