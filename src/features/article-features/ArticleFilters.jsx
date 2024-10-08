65d6ff767b493154d7065cab12009683ea0fb676
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-08 13:19:13 +0100

    update styling

--- src/features/article-features/ArticleFilters.jsx
+++ src/features/article-features/ArticleFilters.jsx
@@ -1,8 +1,9 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 function ArticleFilters() {
   return (
-    <div className="links">
+    <LinkContainer>
       <NavLink
         id={
           location.pathname + location.search === "/admin/articles"
@@ -35,8 +36,39 @@ function ArticleFilters() {
       >
         Unpublished
       </NavLink>
-    </div>
+    </LinkContainer>
   );
 }
+
+const LinkContainer = styled.div`
+  grid-column: 1 / -1;
+  grid-row: 1 / 2;
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
+
+  a {
+    &:hover {
+      background-color: #a5c77e;
+    }
+    transition: 0.3s ease-out;
+    font-weight: 400;
+    color: black;
+    background-color: #8eac6c;
+    padding: 10px;
+    min-width: 50px;
+    text-align: center;
+    box-shadow:
+      rgba(0, 0, 0, 0.12) 0px 1px 3px,
+      rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  }
+
+   a#queryActive {
+    background-color: #ffd159;
+    box-shadow:
+      rgba(0, 0, 0, 0.16) 0px 1px 4px,
+      rgb(51, 51, 51) 0px 0px 0px 3px;
+  }
+`;
 
 export default ArticleFilters;
--- src/features/article-features/ArticlePreview.jsx
+++ src/features/article-features/ArticlePreview.jsx
@@ -1,9 +1,11 @@
 import DOMPurify from "dompurify";
 import parse from "html-react-parser";
 import styled from "styled-components";
+import { Content } from "../../components/sharedStyles";
 
 function ArticlePreview({ inputs }) {
   const { title, body, tagNames } = inputs;
+  const tagCount = tagNames.length;
 
   function sanitize(content) {
     const sanitized = DOMPurify.sanitize(content);
@@ -11,20 +13,77 @@ function ArticlePreview({ inputs }) {
   }
 
   return (
-    <>
-      <header>
-        <h1>{title}</h1>
-      </header>
-      <PreviewSection>
-        {parse(sanitize(body))}
-        {tagNames.map((tag) => (
-          <p key={tag.tagName}>{tag.tagName}</p>
+    <PreviewMain>
+      <PreviewHeader>
+        {tagNames.map((tag, i) => (
+          <span className="tag" key={tag}>
+            {i < tagCount - 1 ? `${tag},` : tag}
+          </span>
         ))}
-      </PreviewSection>
-    </>
+        <h1>{title}</h1>
+      </PreviewHeader>
+      <div className="divider" />
+      <PreviewSection>{parse(sanitize(body))}</PreviewSection>
+    </PreviewMain>
   );
 }
 
-const PreviewSection = styled.section``;
+const PreviewMain = styled.main`
+  ${Content}
+  color: white;
+  row-gap: 20px;
+  justify-items: center;
+
+  .divider {
+    border: 0.75px solid white;
+    width: 25%;
+    height: 0px;
+  }
+`;
+
+const PreviewHeader = styled.header`
+  text-align: center;
+  margin: 50px 0px;
+
+  .tag {
+    color: #abd081;
+    font-size: 1rem;
+    margin-bottom: 15px;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+    font-weight: 300;
+  }
+`;
+
+const PreviewSection = styled.section`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    margin-top: 20px;
+    font-size: 1.6rem;
+    font-weight: 200;
+  }
+
+  ul,
+  ol {
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+    li {
+      margin-left: 25px;
+    }
+  }
+
+  ol {
+    background-color: #898989;
+    border: 0.75px solid white;
+    border-radius: 10px;
+    padding: 10px 25px;
+  }
+`;
 
 export default ArticlePreview;
