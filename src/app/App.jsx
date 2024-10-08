09a3f0e46bd8445813737fff08af2697871a7928
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-08 12:34:07 +0100

    remove user state effect

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,24 +1,9 @@
 import "./App.css";
-import { useContext, useEffect } from "react";
-import { AuthContext } from "../services/authProvider";
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const { user, setUser } = useContext(AuthContext);
-  const userData = useLoaderData();
-
-  //ensures userState not null when client has token
-  useEffect(() => {
-    if (!user && userData) {
-      setUser(userData.user);
-    }
-  }, [user, userData, setUser]);
-
-  //guards from login page flash inbetween page reload and useEffect
-  if(!user && userData) return;
-
   return (
-    <>    
+    <>
       <Outlet />
     </>
   );
