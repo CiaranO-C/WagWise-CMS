2347d620c82f20a945dbe952591db7ca197fa3f2
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-08 12:25:36 +0100

    remove loader logic from file

---
+++ src/app/router/router.jsx
@@ -0,0 +1,84 @@
+import ErrorPage from "../routes/Error.jsx";
+import App from "../App.jsx";
+import Login from "../routes/login.jsx";
+import Home from "../routes/Home.jsx";
+import Protected from "../routes/Protected.jsx";
+import Articles from "../routes/Articles.jsx";
+import Tags from "../routes/Tags.jsx";
+import NewArticle from "../routes/NewArticle.jsx";
+import TagArticles from "../routes/TagArticles.jsx";
+import EditArticle from "../routes/EditArticle.jsx";
+import SearchResults from "../routes/SearchResults.jsx";
+import Comments from "../routes/Comments.jsx";
+import {
+  articlesLoader,
+  commentsLoader,
+  dashboardLoader,
+  taggedArticles,
+  tagsLoader,
+} from "./loaders.js";
+
+const routesConfig = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/admin",
+        element: <Protected />,
+        children: [
+          {
+            path: "home",
+            element: <Home />,
+            loader: dashboardLoader,
+          },
+          {
+            path: "articles",
+            element: <Articles />,
+            loader: articlesLoader,
+            children: [
+              {
+                path: ":id",
+                element: <EditArticle />,
+                loader: tagsLoader,
+              },
+            ],
+          },
+          {
+            path: "tags",
+            element: <Tags />,
+            loader: tagsLoader,
+            children: [
+              {
+                path: ":tagName",
+                element: <TagArticles />,
+                loader: taggedArticles,
+              },
+            ],
+          },
+          {
+            path: "new_article",
+            element: <NewArticle />,
+            loader: tagsLoader,
+          },
+          {
+            path: "search",
+            element: <SearchResults />,
+          },
+          {
+            path: "comments",
+            element: <Comments />,
+            loader: commentsLoader,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default routesConfig;
