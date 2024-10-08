cad3b223e42361cae0a9e79d9244814988331ea1
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-08 12:30:59 +0100

    write effect to update user state, add guard state

--- src/app/routes/Protected.jsx
+++ src/app/routes/Protected.jsx
@@ -1,14 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../services/authProvider";
 import { Navigate, Outlet } from "react-router-dom";
 import styled from "styled-components";
 import Sidebar from "../../components/Sidebar";
+import { userLoader } from "../../services/authService";
 
 function Protected() {
-  const { user } = useContext(AuthContext);
-  if (!user) return <Navigate to="/login" />;
+  const { user, setUser } = useContext(AuthContext);
+  const [guard, setGuard] = useState(true);
 
-console.log("Protected passed -->", user);
+  useEffect(() => {
+    async function getUser() {
+      const user = await userLoader();
+      if (user) setUser(user);
+      setGuard(false);
+    }
+    if (!user) {
+      getUser();
+    } else {
+      setGuard(false);
+    }
+  }, [user, setUser]);
+
+  if (!user && !guard) {
+    return <Navigate to="/login" />;
+  }
+
+  if (guard) return;
 
   return (
     <ProtectedLayout>
@@ -21,7 +39,7 @@ console.log("Protected passed -->", user);
 const ProtectedLayout = styled.main`
 display: grid;
 grid-template-columns: 200px 1fr;
-max-height: 100vh;
+min-height: 100vh;
 }
 `;
 
