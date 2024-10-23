e8f1728cfb4c0df0be53a4e1b3daa3998ba15278
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-23 22:19:15 +0100

    write refresh login in getToken, write check token expiry funciton

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,6 +1,34 @@
-function getToken() {
-  const token = localStorage.getItem("accessToken");
+import { jwtDecode } from "jwt-decode";
+import { refreshToken } from '../services/authService';
+
+async function getToken(abortSignal) {
+  let token = localStorage.getItem("accessToken");
+
+  if ((!token || isTokenExpired(token)) && getRefreshToken()){
+    const isRefreshed = await refreshToken(abortSignal);
+    if(isRefreshed){
+      token = localStorage.getItem("accessToken");
+    } else {
+      //access and refresh bad so throw new response to redirect user to login page
+      console.log("Bad token alert!");
+      
+    }
+  } 
   return token;
+}
+
+function getRefreshToken(){
+  return sessionStorage.getItem("refreshToken");
+}
+
+function isTokenExpired(token) {
+  const { exp } = jwtDecode(token);
+  // Get current time in seconds
+  const currentTime = Math.floor(Date.now() / 1000);
+  const isExpired = exp < currentTime;
+  console.log("isExpired --> ", isExpired);
+  
+  return isExpired;
 }
 
 const API_URL = import.meta.env.VITE_API_URL;
