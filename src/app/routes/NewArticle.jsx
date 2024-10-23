45506f761df4959ad6f3faad0695b5b225f17aec
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-23 22:24:35 +0100

    Remove data fetch

--- src/app/routes/NewArticle.jsx
+++ src/app/routes/NewArticle.jsx
@@ -1,16 +1,13 @@
-import { useLoaderData } from "react-router-dom";
 import { useState } from "react";
 import ArticleEdit from "../../features/article-features/ArticleEdit.jsx";
 
 function NewArticle() {
   const [loading, setLoading] = useState(true);
-  const tags = useLoaderData();
 
   return (
     <ArticleEdit
       loading={loading}
       setLoading={() => setLoading(false)}
-      tags={tags}
       article={null}
     />
   );
